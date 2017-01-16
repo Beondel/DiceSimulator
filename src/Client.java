3d2683a2696046089bca3513e1dea8f57b7816a0
Beondel <bengmacmillan@gmail.com>
2017-01-15 17:58:40 -0800

    MVP finished

--- src/Client.java
+++ src/Client.java
@@ -2,4 +2,16 @@
  * Created by benjaminmacmillan1 on 1/15/17.
  */
 public class Client {
+    public static void main(String[] args) {
+        Dice firstDice = new Dice(6);
+        Dice secondDice = new Dice(6);
+
+        for (;;) {
+            System.out.println("roll is: " + rollDice(firstDice, secondDice));
+        }
+    }
+
+    public static int rollDice(Dice one, Dice two) {
+        return one.roll() + two.roll();
+    }
 }
--- src/Dice.java
+++ src/Dice.java
@@ -1,6 +1,18 @@
 /**
  * Created by benjaminmacmillan1 on 1/15/17.
  */
+import java.util.*;
+
 public class Dice {
-    // I am tryin g
+    private int sides;
+
+    public Dice(int sides) {
+        this.sides = sides;
+    }
+
+    public int roll() {
+        Random side = new Random();
+
+        return side.nextInt(this.sides) + 1;
+    }
 }
